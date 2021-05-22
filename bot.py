232a9a7c587022f4658748e05ec1e97e774d560f
kevinnadar264 <76389377+kevinnadar264@users.noreply.github.com>
2021-05-22 18:14:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ async def link_handler(bot, message):
 
 
 async def get_shortlink(link):
-    url = 'http://pdiskshortner.com'
+    url = 'http://pdiskshortner.com/api'
     params = {'api': API_KEY, 'url': link}
 
     async with aiohttp.ClientSession() as session:
