25439aa079ab61d4b4934fdc9479f954930e51a0
kevinnadar264 <76389377+kevinnadar264@users.noreply.github.com>
2021-05-22 17:46:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ async def link_handler(bot, message):
     link = message.matches[0].group(0)
     try:
         short_link = await get_shortlink(link)
-        await message.reply(f'Here is your <code>{short_link}</code>', quote=True)
+        await message.reply(f'Here is your "<code> {short_link} </code>"', quote=True)
     except Exception as e:
         await message.reply(f'Error: {e}', quote=True)
 
